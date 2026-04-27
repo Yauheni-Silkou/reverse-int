9a65080e5442f0ee9390b075d82c51bd8c1c00c7
Yauheni-Silkou <yauheni.silkou@outlook.com>
2026-04-27 21:34:58 +0300

    feat: implement the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const num = n < 0 ? -n : n;
+  return +[...`${num}`].reverse().join('');
 };
